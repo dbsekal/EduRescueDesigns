4a5076d3bfcce8b289cd9dd8822cd701fd940f72
kaylatran <trankayla7@gmail.com>
2024-03-06 21:23:53 -0800

    Sign Up Form, needs more work

--- app/src/main/java/com/example/edurescuedesigns/Register.kt
+++ app/src/main/java/com/example/edurescuedesigns/Register.kt
@@ -1,19 +1,31 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.edurescuedesigns
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.ExposedDropdownMenuDefaults.TrailingIcon
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -34,10 +46,14 @@ fun RegisterForm(navController: NavController) {
     val email = remember { mutableStateOf("")}
     val password = remember { mutableStateOf("")}
     val studentID = remember { mutableStateOf("")}
+    val userSelection = remember { mutableStateOf("") }
+    val scrollState = rememberScrollState()
+
 
     Column(modifier = Modifier
         .fillMaxSize()
-        .padding(40.dp),
+        .padding(8.dp)
+        .verticalScroll(scrollState),
         verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.Start
 
@@ -51,11 +67,18 @@ fun RegisterForm(navController: NavController) {
         Text(
             modifier = Modifier.width(250.dp),
             text = "Create a new account",
-            fontSize = 40.sp,
+            fontSize = 35.sp,
             fontWeight = FontWeight.Bold
         )
         FirstNameField(firstName.value) {firstName.value = it}
         LastNameField(lastName.value) {lastName.value = it}
+        RegisterEmailField(email.value) {email.value = it}
+        RegisterPasswordField(password.value) {password.value = it}
+        DropDownUser(userSelection.value){userSelection.value = it}
+        IDField(studentID.value) {studentID.value = it}
+        RegisterButton()
+
+
 
     }
 
@@ -90,7 +113,7 @@ fun LastNameField(
         value = lastNameValue,
         onValueChange = onLastNameChange,
         modifier = Modifier.padding(8.dp),
-        label = { Text(text ="First Name") },
+        label = { Text(text ="Last Name") },
         keyboardOptions = KeyboardOptions.Default.copy(
             keyboardType = KeyboardType.Text,
             imeAction = ImeAction.Next
@@ -101,28 +124,127 @@ fun LastNameField(
     )
 }
 
-//@Composable
-//fun EmailField(
-//    emailValue: String,
-//    onEmailChange = (String) -> Unit
-//){
-//    OutlinedTextField(
-//        value = emailValue,
-//        onValueChange = onEmailChange,
-//        modifier = Modifier.padding(8.dp),
-//        label = { Text(text ="Email") },
-//        keyboardOptions = KeyboardOptions.Default.copy(
-//            keyboardType = KeyboardType.Email,
-//            imeAction = ImeAction.Next
-//        ),
-//        keyboardActions = KeyboardActions(
-//            onNext = {}
-//        )
-//    )
-//}
 @Composable
-fun RegisterButton(email: String, password: String,navController: NavController) {
-    Button(onClick = {submitLogin(email,password, navController)}) {
+fun RegisterEmailField(
+    emailValue: String,
+    onEmailChange: (String) -> Unit
+)
+{
+    OutlinedTextField(
+        value = emailValue,
+        onValueChange = onEmailChange,
+        modifier = Modifier.padding(8.dp),
+        label = { Text(text ="Email") },
+        keyboardOptions = KeyboardOptions.Default.copy(
+            keyboardType = KeyboardType.Email,
+            imeAction = ImeAction.Next
+        ),
+        keyboardActions = KeyboardActions(
+            onNext = {}
+        )
+    )
+}
+
+@Composable
+fun RegisterPasswordField(
+    passwordValue: String,
+    onPasswordChange: (String) -> Unit
+)
+{
+    OutlinedTextField(
+        value = passwordValue,
+        onValueChange = onPasswordChange,
+        modifier = Modifier.padding(8.dp),
+        label = { Text(text ="Password") },
+        keyboardOptions = KeyboardOptions.Default.copy(
+            keyboardType = KeyboardType.Password,
+            imeAction = ImeAction.Next
+        ),
+        keyboardActions = KeyboardActions(
+            onNext = {}
+        )
+    )
+}
+
+@Composable
+fun DropDownUser(
+    userValue: String,
+    onUserValueChange: (String) -> Unit
+) {
+    val isExpanded = remember { mutableStateOf(false) }
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+    ) {
+        ExposedDropdownMenuBox(
+            expanded = isExpanded.value,
+            onExpandedChange = { isExpanded.value = it }
+        ) {
+            TextField(
+                value = userValue,
+                onValueChange = {onUserValueChange},
+                readOnly = true,
+                trailingIcon = {
+                    TrailingIcon(expanded = isExpanded.value)
+                },
+                colors = ExposedDropdownMenuDefaults.textFieldColors()
+            )
+
+            ExposedDropdownMenu(
+                expanded = isExpanded.value,
+                onDismissRequest = { isExpanded.value = false }
+            ) {
+                DropdownMenuItem(
+                    text = { Text("Admin") },
+                    onClick = {
+                        onUserValueChange("Admin")
+                        isExpanded.value = false
+                    }
+                )
+                DropdownMenuItem(
+                    text = { Text("Teacher") },
+                    onClick = {
+                        onUserValueChange("Teacher")
+                        isExpanded.value = false
+                    }
+                )
+                DropdownMenuItem(
+                    text = { Text("Student") },
+                    onClick = {
+                        onUserValueChange("Student")
+                        isExpanded.value = false
+                    }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun IDField(
+    iDValue: String,
+    onIDChange: (String) -> Unit
+)
+{
+    OutlinedTextField(
+        value = iDValue,
+        onValueChange = onIDChange,
+        modifier = Modifier.padding(8.dp),
+        label = { Text(text ="ID") },
+        keyboardOptions = KeyboardOptions.Default.copy(
+            keyboardType = KeyboardType.Text,
+            imeAction = ImeAction.Next
+        ),
+        keyboardActions = KeyboardActions(
+            onNext = {}
+        )
+    )
+}
+
+@Composable
+fun RegisterButton() {
+    Button(onClick = {/*todo*/}) {
         Text("Sign Up")
     }
 }
