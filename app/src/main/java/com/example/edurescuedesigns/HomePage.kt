ca5ad11b6bfd6f5979c4a5e045abdac6dd7f10d6
kaylatran <trankayla7@gmail.com>
2024-03-11 22:13:21 -0700

    title on homepage

--- app/src/main/java/com/example/edurescuedesigns/HomePage.kt
+++ app/src/main/java/com/example/edurescuedesigns/HomePage.kt
@@ -1,7 +1,14 @@
 package com.example.edurescuedesigns
 import android.util.Log
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
@@ -9,6 +16,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.edurescuedesigns.classes.Network
 import com.example.edurescuedesigns.datatypes.EmergencyPlan
@@ -61,7 +74,27 @@ fun HomePageForm(navController:NavController,shouldShowBottomBar: MutableState<B
         }
 
     }
+    topBar()
     Text(emergencyString)
 
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun topBar(){
+    TopAppBar(
+        title = {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+                Text(text ="EduRescue", color = Color(0xFF930005), fontSize = 25.sp, fontFamily = FontFamily.Default, textAlign = TextAlign.Center)
+            }
+        },
+        actions = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(Icons.Filled.AccountCircle, contentDescription = "User Profile")
+
+            }
+        }
+
+
+    )
+}
